c5334c6ef26379eb378b03acb38de4dd7eeb0b05
yasodha2008 <yasodham.ug.25.ec@francisxavier.ac.in>
2026-09-03 22:00:12 +0530

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -1167,10 +1167,9 @@ def admin_logout():
 # =========================================================
 
 if __name__ == "__main__":
-
     app.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=5000,
-        debug=True
+        debug=False
     )
 
